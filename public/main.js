b28e38f43fdcca81725890b7b8a02c95088254af
Olivia Giandrea <olivia.giandrea@gmail.com>
2023-05-22 22:43:17 -0400

    adding navbar

--- public/main.js
+++ public/main.js
@@ -1,3 +1,31 @@
+// ======================================================================================
+
+const nav = document.querySelector(".nav");
+const searchIcon = document.querySelector("#searchIcon");
+const navOpenBtn = document.querySelector(".navOpenBtn");
+const navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("fa-magnifying-glass", "fa-xmark");
+  }
+  searchIcon.classList.replace("fa-xmark", "fa-magnifying-glass");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("fa-xmark", "fa-magnifying-glass");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+
+// ======================================================================================
+
 $('#addIng').on('click', function(event) {
   event.preventDefault();
   var $ing = $('#ingredients').one();
